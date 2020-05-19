d1bc2848c788fc5de0190220e61c89693332520c
snailencholy <paul.hampton1992@gmail.com>
2020-05-19 10:06:34 -0500

    First

--- ssa-judges/src/App.js
+++ ssa-judges/src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TextField from '@material-ui/core/TextField';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <TextField id="filled-search" label="Search Judges" type="Search" variant="filled"/>
+      </div>
     </div>
   );
 }
